56fed578cd8bba4ebcb7cb06f1c62545b6a2d5d7
Berrios Vazquez <k.berrios.vazquez@accenturefederal.com>
2020-05-28 17:03:27 -0400

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
-=======
   {
   	path: '',
   	redirectTo: '/workspace/user-directory', pathMatch: 'full'
@@ -16,7 +14,6 @@ const routes: Routes = [
   	path: '**',
   	redirectTo: '/workspace/user-directory', pathMatch: 'full'
   }
->>>>>>> 2d1ad814502b1d39cd2afc2d5ab618d7712b8598
 ];
 
 @NgModule({
